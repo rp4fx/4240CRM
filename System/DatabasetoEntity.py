adb974fce369ec303121e5641ebca81cd2d39f76
Timur <tia4kj@virginia.edu>
2013-12-04 22:51:21 -0500

    Corrected EntityToDatabase method names

--- System/DatabasetoEntity.py
+++ System/DatabasetoEntity.py
@@ -10,6 +10,94 @@ from System.Entities.Relationship import *
 from System.Entities.Message import *
 
 
+class DatabaseToEntity:
+    def __init__(self, db):
+        self.entities = []
+        self.db = db
+        self.attribute_table_getters = []
+
+    def connect_to_database(self):
+        self.conn = sqlite3.connect(self.db)
+        self.cursor = self.conn.cursor()
+
+    def close_db_connection(self):
+        self.conn.commit()
+        self.conn.close()
+
+    def add_attribute_table_getters(self, attribute_table_getter):
+        self.attribute_table_getters.append(attribute_table_getter)
+
+class DatabaseToPerson (DatabaseToEntity):
+    def get_people_from_database(self):
+        self.connect_to_database()
+        self.query_database()
+        for person in self.entities:
+            personid = self.insert_person(person)
+            print "Inserted person with id %s" %(personid)
+            for entity_to_attribute_table in self.attribute_table_setters:
+                entity_to_attribute_table.add_to_table(person, personid, self.cursor)
+        self.close_db_connection()
+
+    def query_database(self):
+        self.cursor.execute("SElECT * from person")
+        rows_from_db = return_rows_db(self.cursor)
+        for row in rows_from_db:
+            p = Person()
+            p.person_id = row[0]
+            p.first_name = row[1]
+            p.last_name = row[2]
+            p.other_name = row[3]
+            p.birthday = row[4]
+            p.gender = row[5]
+            p.note = row[6]
+
+        people_list =[]
+        self.curs.execute("SELECT address from email where personid = %d " %(self.PID))
+        email_list = return_rows_db(self.curs)
+        self.curs.execute("SELECT number from phone where personid = %s" % (self.PID))
+        phone_list = return_rows_db(self.curs)
+        for p in people_list_unmade:
+            person = Person(p[0],p[1],p[2],p[3],p[4],p[5],p[6],email_list,phone_list)
+            people_list.append(person)
+
+    def return_rows_db(self, cursor):
+        ret = []
+        rows = cursor.fetchall()
+        for r in rows:
+            ret.append(r)
+        return ret
+
+class AttributeTableGetter:
+    def __init__(self, db):
+        self.db = db
+        self.opened_connection = False
+
+    def connect_to_database(self):
+        self.conn = sqlite3.connect(self.db)
+        self.cursor = self.conn.cursor()
+        self.opened_connection = True
+
+    def close_db_connection(self):
+        self.conn.commit()
+        self.conn.close()
+
+class PhoneAttributeTableGetter(AttributeTableGetter):
+    def get_attribute(self, person, personid, cursor=None):
+        self.cursor = cursor
+        if self.cursor is None:
+            self.connect_to_database()
+        self.cursor.execute("SELECT emailid, address from email where personid = %d " %(personid))
+        rows_from_db = return_rows_db(self.cursor)
+        for row in rows_from_db:
+            e = Email()
+            e
+        if self.opened_connection:
+            self.close_db_connection()
+
+
+
+
+
 def return_rows_db(curs):
     ret = []
     rows = curs.fetchall()

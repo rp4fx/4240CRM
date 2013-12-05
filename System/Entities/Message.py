3cfbd5809e0ce5142b562be5a6d2318fe87b5f4a
avh2ag <alex.harasty@gmail.com>
2013-12-04 21:39:37 -0500

    Finished logic for processing people up to the pushing to db

--- System/Entities/Message.py
+++ System/Entities/Message.py
@@ -7,8 +7,14 @@ class Message:
         self.messageid = messageid
         self.content = content
         self.timestamp = timestamp
-
+        self.people = {"to": [], "from": [], "cc": [], "bcc": []}
 
     def __str__(self):
         return "Message content: "+ self.content+"\nSent: "+self.timestamp
 
+    #@people == to, from, cc/bcc
+    def set_people(self, information):
+        self.people["to"] = information["to"]
+        self.people["from"] = information["from"]
+        self.people["cc"] = information["cc"]
+        self.people["bcc"] = information["bcc"]

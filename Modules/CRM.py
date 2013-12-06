4c269b2bdcdf304e76222f3a3d9469d11cd6a7f7
Timur <tia4kj@virginia.edu>
2013-12-06 02:54:09 -0500

    CRM Module now has popups for messages

--- Modules/CRM.py
+++ Modules/CRM.py
@@ -69,140 +69,7 @@ class CRM(Frame):
         return Person()
 
     def create_person_popup(self, person):
-        top = Toplevel(height="1000", width="1000")
-        self.add_person_details(person, top)
-        self.add_messages(person, top)
-
-    def create_person_details_paned_window(self, parent):
-        user_details_paned_window = PanedWindow(parent, orient=HORIZONTAL)
-        user_details_paned_window.pack(fill=BOTH, expand=1)
-        return user_details_paned_window
-
-    def create_person_details_frame(self, paned_window):
-        user_details_frame = Frame(paned_window, borderwidth=5)
-        paned_window.add(user_details_frame)
-        self.configure_columns(user_details_frame)
-        self.configure_rows(user_details_frame)
-        return user_details_frame
-
-    def configure_columns(self, user_details_frame):
-        user_details_frame.columnconfigure(0, pad=3)
-        user_details_frame.columnconfigure(1, pad=3)
-        user_details_frame.columnconfigure(2, pad=3)
-        user_details_frame.columnconfigure(3, pad=3)
-        user_details_frame.columnconfigure(4, pad=3)
-
-    def configure_rows(self, user_details_frame):
-        user_details_frame.rowconfigure(0, pad=3)
-        user_details_frame.rowconfigure(1, pad=3)
-        user_details_frame.rowconfigure(2, pad=3)
-        user_details_frame.rowconfigure(3, pad=3)
-        user_details_frame.rowconfigure(4, pad=3)
-        user_details_frame.rowconfigure(5, pad=3)
-        user_details_frame.rowconfigure(6, pad=3)
-
-    def add_person_details(self, person, top_level):
-        user_details_paned_window = self.create_person_details_paned_window(top_level)
-        user_details_frame = self.create_person_details_frame(user_details_paned_window)
-
-        img_file = "user-icon.gif"
-        img = PhotoImage(file=img_file, width=100, height=100)
-        image_label = Label(user_details_frame, image=img)
-        image_label.grid(row=0, column=0, rowspan=7)
-        image_label.image = img
-
-        name_label = Label(user_details_frame, text=str(person.first_name) + " " + str(person.last_name))
-        name_label.grid(row=0, column=2, columnspan=2, sticky=W+E)
-
-        other_name_label = Label(user_details_frame, text=str(person.other_name))
-        other_name_label.grid(row=1, column=2, columnspan=2, sticky=W+E)
-
-        birthday_label = Label(user_details_frame, text="Birthday:")
-        birthday_label.grid(row=3, column=2, sticky=W+E)
-
-        actual_birthday_label = Label(user_details_frame, text=str(person.birthday))
-        actual_birthday_label.grid(row=3, column=3, sticky=W+E)
-
-        gender_label = Label(user_details_frame, text="Gender:")
-        gender_label.grid(row=4, column=2, sticky=W+E)
-
-        actual_gender_label = Label(user_details_frame, text=str(person.gender))
-        actual_gender_label.grid(row=4, column=3, sticky=W+E)
-
-        email_label = Label(user_details_frame, text="Emails:")
-        email_label.grid(row=5, column=2, sticky=W+E+N)
-
-        actual_email_label = Label(user_details_frame)
-        actual_email_label.grid(row=5, column=3, sticky=W+E)
-
-        for email in person.emails:
-            temp_email_label = Label(actual_email_label, text=str(email.address), anchor=E)
-            temp_email_label.config(anchor=E)
-            temp_email_label.pack()
-
-        phone_label = Label(user_details_frame, text="Phones:")
-        phone_label.grid(row=6, column=2, sticky=W+E+N)
-
-        actual_phone_label = Label(user_details_frame)
-        actual_phone_label.grid(row=6, column=3, sticky=W+E)
-
-        for phone in person.phones:
-            temp_phone_label = Label(actual_phone_label, text=str(phone.type) + " - " + str(phone.number), anchor=E)
-            temp_phone_label.config(anchor=E)
-            temp_phone_label.pack()
-
-        self.add_notes_to_person_details(user_details_paned_window, person)
-
-    def add_notes_to_person_details(self, user_details_paned_window, person):
-        note_frame = Frame(user_details_paned_window, height=200)
-        user_details_paned_window.add(note_frame)
-        #label
-        note_label = Label(note_frame, text="Notes")
-        note_label.pack()
-        #scrollbar
-        note_scrollbar = Scrollbar(note_frame)
-        note_scrollbar.pack(side=RIGHT, fill=Y)
-        #text
-        note_text = Text(note_frame, yscrollcommand=note_scrollbar.set, height=10)
-        note_text.insert(END, person.note)
-        note_text.pack()
-        note_scrollbar.config(command=note_text.yview)
-
-    def add_messages(self, person, top_level):
-        messages_frame = Frame(top_level)
-        messages_frame.pack(fill=BOTH, expand=1)
-        message_tree = self.create_message_tree(messages_frame)
-        self.add_messages_to_tree(person, message_tree)
-        message_tree.bind("<<TreeviewSelect>>", self.message_tree_message_click_handler)
-
-    def create_message_tree(self, message_frame):
-        columns = ('Source', 'Subject', 'Date')
-        message_tree = Treeview(message_frame, columns=columns)
-        message_tree.pack(fill=BOTH, expand=1)
-        message_tree.column('#0', width=15)
-        self.add_click_handlers_to_tree_column_headers(message_tree, columns, self.message_tree_header_click_handler)
-        #self.add_scrollbars(message_tree)
-        return message_tree
-
-    def add_messages_to_tree(self, person, message_tree):
-        messages_tree_has_messages = False
-        for relationship in ["TO", "FROM", "CC", "BCC"]:
-            if relationship in person.relationships.keys():
-                for message in person.relationships[relationship]:
-                    messages_tree_has_messages = True
-                    message_tree.insert('', 'end', str(message.messageid), text=str(message.messageid), tags=('#message'), values=("message.source", str(message.subject), str(message.timestamp)))
-                    #temp_message_label = Label(messages_frame, text=str(message.subject))
-                    #temp_message_label.pack(fill=BOTH)
-
-        if not messages_tree_has_messages:
-            message_tree.insert('', 'end', "-1", text="no messages", tags=('#no_messages'), values=("", "No messages available", ""))
-
-    def message_tree_header_click_handler(self, event):
-        print "Clicked a message tree header!"
-
-    def message_tree_message_click_handler(self, event):
-        print "Clicked a message!"
-
+        PersonPopUp(person, self.messages)
 
     def add_data_to_tree(self, tree):
         #self.get_people_and_messages()
@@ -251,6 +118,269 @@ class CRM(Frame):
         quitButton = Button(self, text="Quit", command=self.quit)
         quitButton.place(x=50, y=50)
 
+
+class PersonPopUp:
+    def __init__(self, person, messages):
+        self.top_level = Toplevel(height="1000", width="1000")
+        self.person = person
+        self.messages = messages
+        self.init_pop_up()
+
+    def init_pop_up(self):
+        user_details_paned_window = self.create_person_details_paned_window(self.top_level)
+        user_details_frame = self.create_person_details_frame(user_details_paned_window)
+        self.add_person_details(user_details_frame)
+        self.add_notes_to_paned_window(user_details_paned_window, self.person)
+        self.add_messages(self.person)
+
+    def create_person_details_paned_window(self, parent):
+        user_details_paned_window = PanedWindow(parent, orient=HORIZONTAL)
+        user_details_paned_window.pack(fill=BOTH, expand=1)
+        return user_details_paned_window
+
+    def create_person_details_frame(self, paned_window):
+        user_details_frame = Frame(paned_window, borderwidth=5)
+        paned_window.add(user_details_frame)
+        self.configure_columns(user_details_frame)
+        self.configure_rows(user_details_frame)
+        return user_details_frame
+
+    def configure_columns(self, user_details_frame):
+        user_details_frame.columnconfigure(0, pad=3)
+        user_details_frame.columnconfigure(1, pad=3)
+        user_details_frame.columnconfigure(2, pad=3)
+        user_details_frame.columnconfigure(3, pad=3)
+        user_details_frame.columnconfigure(4, pad=3)
+
+    def configure_rows(self, user_details_frame):
+        user_details_frame.rowconfigure(0, pad=3)
+        user_details_frame.rowconfigure(1, pad=3)
+        user_details_frame.rowconfigure(2, pad=3)
+        user_details_frame.rowconfigure(3, pad=3)
+        user_details_frame.rowconfigure(4, pad=3)
+        user_details_frame.rowconfigure(5, pad=3)
+        user_details_frame.rowconfigure(6, pad=3)
+
+    def add_person_details(self, user_details_frame):
+        self.add_profile_image(user_details_frame)
+        self.add_name(user_details_frame)
+        self.add_other_name(user_details_frame)
+        self.add_birthday(user_details_frame)
+        self.add_gender(user_details_frame)
+        self.add_emails(user_details_frame)
+        self.add_phones(user_details_frame)
+
+    def add_profile_image(self, user_details_frame):
+        img_file = "user-icon.gif"
+        img = PhotoImage(file=img_file, width=100, height=100)
+        image_label = Label(user_details_frame, image=img)
+        image_label.grid(row=0, column=0, rowspan=7)
+        image_label.image = img
+
+    def add_name(self, user_details_frame):
+        name_label = Label(user_details_frame, text=str(self.person.first_name) + " " + str(self.person.last_name))
+        name_label.grid(row=0, column=2, columnspan=2, sticky=W+E)
+
+    def add_other_name(self, user_details_frame):
+        other_name_label = Label(user_details_frame, text=str(self.person.other_name))
+        other_name_label.grid(row=1, column=2, columnspan=2, sticky=W+E)
+
+    def add_birthday(self, user_details_frame):
+        birthday_label = Label(user_details_frame, text="Birthday:")
+        birthday_label.grid(row=3, column=2, sticky=W+E)
+        actual_birthday_label = Label(user_details_frame, text=str(self.person.birthday))
+        actual_birthday_label.grid(row=3, column=3, sticky=W+E)
+
+    def add_gender(self, user_details_frame):
+        gender_label = Label(user_details_frame, text="Gender:")
+        gender_label.grid(row=4, column=2, sticky=W+E)
+        actual_gender_label = Label(user_details_frame, text=str(self.person.gender))
+        actual_gender_label.grid(row=4, column=3, sticky=W+E)
+
+    def add_emails(self, user_details_frame):
+        email_label = Label(user_details_frame, text="Emails:")
+        email_label.grid(row=5, column=2, sticky=W+E+N)
+        actual_email_label = Label(user_details_frame)
+        actual_email_label.grid(row=5, column=3, sticky=W+E)
+        for email in self.person.emails:
+            temp_email_label = Label(actual_email_label, text=str(email.address), anchor=E)
+            temp_email_label.config(anchor=E)
+            temp_email_label.pack()
+
+    def add_phones(self, user_details_frame):
+        phone_label = Label(user_details_frame, text="Phones:")
+        phone_label.grid(row=6, column=2, sticky=W+E+N)
+        actual_phone_label = Label(user_details_frame)
+        actual_phone_label.grid(row=6, column=3, sticky=W+E)
+        for phone in self.person.phones:
+            temp_phone_label = Label(actual_phone_label, text=str(phone.type) + " - " + str(phone.number), anchor=E)
+            temp_phone_label.config(anchor=E)
+            temp_phone_label.pack()
+
+    def add_notes_to_paned_window(self, user_details_paned_window, person):
+        note_frame = Frame(user_details_paned_window, height=200)
+        user_details_paned_window.add(note_frame)
+        #label
+        note_label = Label(note_frame, text="Notes")
+        note_label.pack()
+        #scrollbar
+        note_scrollbar = Scrollbar(note_frame)
+        note_scrollbar.pack(side=RIGHT, fill=Y)
+        #text
+        note_text = Text(note_frame, yscrollcommand=note_scrollbar.set, height=10)
+        note_text.insert(END, person.note)
+        note_text.pack()
+        note_scrollbar.config(command=note_text.yview)
+
+    def add_messages(self, person):
+        messages_frame = Frame(self.top_level)
+        messages_frame.pack(fill=BOTH, expand=1)
+        self.message_tree = self.create_message_tree(messages_frame)
+        self.add_messages_to_tree(person, self.message_tree)
+        self.message_tree.bind("<<TreeviewSelect>>", self.message_tree_message_click_handler)
+
+    def create_message_tree(self, message_frame):
+        columns = ('Source', 'Subject', 'Date')
+        message_tree = Treeview(message_frame, columns=columns)
+        message_tree.pack(fill=BOTH, expand=1)
+        message_tree.column('#0', width=15)
+        self.add_click_handlers_to_tree_column_headers(message_tree, columns, self.message_tree_header_click_handler)
+        #self.add_scrollbars(message_tree)
+        return message_tree
+
+    def add_click_handlers_to_tree_column_headers(self, tree, columns, click_handler):
+        for col in columns:
+            tree.heading(col, text=col, command=partial(click_handler, col))
+
+    def add_messages_to_tree(self, person, message_tree):
+        messages_tree_has_messages = False
+        for relationship in ["TO", "FROM", "CC", "BCC"]:
+            if relationship in person.relationships.keys():
+                for message in person.relationships[relationship]:
+                    messages_tree_has_messages = True
+                    message_tree.insert('', 'end', str(message.messageid), text=str(message.messageid), tags=('#message'), values=("message.source", str(message.subject), str(message.timestamp)))
+                    #temp_message_label = Label(messages_frame, text=str(message.subject))
+                    #temp_message_label.pack(fill=BOTH)
+
+        if not messages_tree_has_messages:
+            message_tree.insert('', 'end', "-1", text="no messages", tags=('#no_messages'), values=("", "No messages available", ""))
+
+    def message_tree_header_click_handler(self, event):
+        print "Clicked a message tree header!"
+
+    def message_tree_message_click_handler(self, event):
+        print "Clicked a message!"
+        message_id = str(self.message_tree.focus())
+        if message_id is not "no messages":
+            message = self.get_message_by_id(message_id)
+            self.create_message_popup(message)
+            print str(self.message_tree.focus())
+
+    def get_message_by_id(self, message_id):
+        print "searching for message with message id: %s" % message_id
+        for m in self.messages:
+            if str(m.messageid) == message_id:
+                print "Found message :)"
+                return m
+        print "Did not find message :("
+        return Message("", "", "")
+
+    def create_message_popup(self, message):
+        MessagePopUp(message)
+
+
+class MessagePopUp:
+    def __init__(self, message):
+        self.top_level = Toplevel(height="1000", width="1000")
+        self.message = message
+        self.init_pop_up()
+
+    def init_pop_up(self):
+        message_details_frame = self.create_message_details_frame(self.top_level)
+        self.add_message_header(message_details_frame)
+        self.add_subject(message_details_frame)
+        self.add_message_content(message_details_frame)
+
+    def create_message_details_frame(self, parent):
+        message_details_frame = Frame(parent, borderwidth=5)
+        message_details_frame.pack(fill=BOTH, expand=1)
+        self.configure_columns(message_details_frame)
+        self.configure_rows(message_details_frame)
+        return message_details_frame
+
+    def configure_columns(self, message_details_frame):
+        message_details_frame.columnconfigure(0, pad=3)
+        message_details_frame.columnconfigure(1, pad=3)
+        message_details_frame.columnconfigure(2, pad=3)
+        message_details_frame.columnconfigure(4, pad=3)
+        message_details_frame.columnconfigure(5, pad=3)
+        message_details_frame.columnconfigure(6, pad=3)
+
+    def configure_rows(self, message_details_frame):
+        message_details_frame.rowconfigure(0, pad=3)
+        message_details_frame.rowconfigure(1, pad=3)
+        message_details_frame.rowconfigure(2, pad=3)
+        message_details_frame.rowconfigure(3, pad=3)
+        message_details_frame.rowconfigure(4, pad=3)
+        message_details_frame.rowconfigure(5, pad=3)
+        #message_details_frame.rowconfigure(6, pad=3)
+
+    def add_message_header(self, message_details_frame):
+        self.add_from(message_details_frame)
+        self.add_to(message_details_frame)
+        self.add_cc(message_details_frame)
+        self.add_bcc(message_details_frame)
+
+    def add_from(self, message_details_frame):
+        from_label = Label(message_details_frame, text="From:")
+        from_label.grid(row=0, column=0, sticky=W+E)
+        if len(self.message.people["FROM"]) > 0:
+            actual_to_label= Label(message_details_frame, text=str(self.format_person(self.message.people["FROM"][0])))
+        else:
+            actual_to_label= Label(message_details_frame, text="")
+        actual_to_label.grid(row=0, column=1, sticky=W+E)
+
+    def add_to(self, message_details_frame):
+        to_label = Label(message_details_frame, text="To:")
+        to_label.grid(row=1, column=0, sticky=W+E)
+        actual_to_label = Label(message_details_frame)
+        actual_to_label.grid(row=1, column=1, sticky=W+E)
+        for person in self.message.people["TO"]:
+            temp_to_label = Label(actual_to_label, text=str(self.format_person(person)), anchor=E)
+            temp_to_label.config(anchor=E)
+            temp_to_label.pack()
+
+    def add_cc(self, message_details_frame):
+        cc_label = Label(message_details_frame, text="CC:")
+        cc_label.grid(row=2, column=0, sticky=W+E)
+        actual_cc_label = Label(message_details_frame)
+        actual_cc_label.grid(row=2, column=1, sticky=W+E)
+        for person in self.message.people["CC"]:
+            temp_label = Label(actual_cc_label, text=str(self.format_person(person)), anchor=E)
+            temp_label.config(anchor=E)
+            temp_label.pack()
+
+    def add_bcc(self, message_details_frame):
+        bcc_label = Label(message_details_frame, text="BCC:")
+        bcc_label.grid(row=3, column=0, sticky=W+E)
+        actual_bcc_label = Label(message_details_frame)
+        actual_bcc_label.grid(row=3, column=1, sticky=W+E)
+        for person in self.message.people["BCC"]:
+            temp_label = Label(actual_bcc_label, text=str(self.format_person(person)), anchor=E)
+            temp_label.config(anchor=E)
+            temp_label.pack()
+
+    def add_subject(self, message_details_frame):
+        subject_label = Label(message_details_frame, text=str(self.message.subject))
+        subject_label.grid(row=4, column=0, columnspan=6, sticky=W+E)
+
+    def add_message_content(self, message_details_frame):
+        content_text = Text(message_details_frame)
+        content_text.insert(END, self.message.content)
+        content_text.grid(row=5, column=0, columnspan=6, sticky=N+S+E+W)
+
+    def format_person(self, person):
+        return str(person.first_name) + " " + str(person.last_name)
 
 def main():
     print 'do something'
